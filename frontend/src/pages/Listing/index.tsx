2aafb778dbe2a24e353daf0a88c5aa9d1d4d7ec9
Romulo <romulomesquita.06@gmail.com>
2022-03-09 00:12:05 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,4 +1,5 @@
-import MovieScore from "components/MovieScore";
+import MovieCard from "components/MovieCard";
+/* import MovieScore from "components/MovieScore"; */
 /* import MovieStars from "components/MovieStars"; */
 import Pagination from "components/Pagination";
 
@@ -6,7 +7,31 @@ function Listing() {
     return (
         <>
             <Pagination />
-            <MovieScore/>
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+
+
+                </div>
+            </div>
+
+
+
         </>
 
     )
